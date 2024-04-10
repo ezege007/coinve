87834cfb70925f4f6d6ee6a4cb09860d2e93e9ee
caseykingsley77 <caseykingsley77@gmail.com>
2024-04-10 18:59:12 +0100

    bu

--- coinvestor/build_files.sh
+++ coinvestor/build_files.sh
@@ -1,3 +1,14 @@
+ [[source]]
+url = "https://pypi.org/simple"
+verify_ssl = true
+name = "pypi"
+ 
+[packages]
+django = "*"
+ 
+[requires]
+python_version = "3.9"
+ 
  echo "BUILD START"
  python3.7 -m pip install -r requirements.txt
  python3.7 manage.py collectstatic --noinput --clear
