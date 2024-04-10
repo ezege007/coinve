cc57e4868e1872a4f7420df043a3915f323a18f4
caseykingsley77 <caseykingsley77@gmail.com>
2024-04-10 18:41:51 +0100

    build

--- coinvestor/build_files.sh
+++ coinvestor/build_files.sh
@@ -1,4 +1,9 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+echo "BUILD START"
+
+# Install requirements
+python3.9 -m pip install -r /coinvestor/requirements.txt
+
+# Collect static files
+python3.9 /coinvestor/manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
